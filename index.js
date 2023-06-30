39501a8d4c121fc58e91e6e687e49612e760e13d
Sean <95305272+seanscodes@users.noreply.github.com>
2023-06-30 20:36:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -153,4 +153,4 @@ client.on('message', async message => {
 })
  
 
-client.login("MTA2MzQwNTMxNDczNDU1OTIzMg.GNi7bJ.XBRV8GCpIpKHdQ3VR_BK5Es9eErNsRmq-CNJk8"); //Add the token to your bot user here
+client.login("MTA2MzQwNTMxNDczNDU1OTIzMg.GbhNeh.V1vBEX8ReobBQuab18QruDeOtu28ClUoBAZBZY"); //Add the token to your bot user here
